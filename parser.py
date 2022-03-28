0bd338922fd24fa19ef8d45bfe9837d232831166
yarosshest <yarosshest@gmail.com>
2022-03-28 03:06:14 +0300

    Successful parsing id films

--- parser.py
+++ parser.py
@@ -1,7 +1,7 @@
 from kinopoisk_unofficial.kinopoisk_api_client import KinopoiskApiClient
 from kinopoisk_unofficial.request.films.film_request import FilmRequest
 from constants import TOKEN
-from Database import DataFunFilm
+from Database import DataFunFilm,DataFunId
 import time
 
 
@@ -12,7 +12,6 @@ def parser():
     n = last_id + 1
     for i in range(last_id, n):
         try:
-            time.sleep(0.1)
             request = FilmRequest(i)
             response = api_client.films.send_film_request(request)
             response.film.genres
@@ -20,9 +19,27 @@ def parser():
                 print('Added {}'.format(response.film.kinopoisk_id))
             else:
                 print('Id is already in the database {}'.format(response.film.kinopoisk_id))
+        except Exception as error:
+            print('Id is not capable ' + str(error))
 
+
+def parserInListId():
+    api_client = KinopoiskApiClient(TOKEN)
+    filmDB = DataFunFilm()
+    idDB = DataFunId()
+    id_lst = idDB.get_id_to_pars()
+    for i in id_lst:
+        try:
+            request = FilmRequest(i)
+            response = api_client.films.send_film_request(request)
+            response.film.genres
+            if filmDB.addFilm(response):
+                print('Added {}'.format(i))
+            else:
+                print('Id is already in the database {}'.format(i))
+            idDB.id_update(i)
         except Exception as error:
             print('Id is not capable ' + str(error))
             
 if __name__ == '__main__':
-    parser()
+    parserInListId()
