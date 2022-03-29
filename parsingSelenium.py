3c8f68374c7eb8f9d2a9a0052ebbc2e4946dd1f9
Kovel Alexander <xanderkovel@gmail.com>
2022-03-29 22:48:38 +0300

    Added fixed kinpoisk api, fixed is parser

--- parsingSelenium.py
+++ parsingSelenium.py
@@ -2,12 +2,13 @@ import time
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from Database import DataFunId
+from webdriver_manager.chrome import ChromeDriverManager
 
 options = webdriver.ChromeOptions()
 options.binary_location = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
 chrome_driver_binary = "D:\chromedriver_win32\chromedriver.exe"
-# driver = webdriver.Chrome(ChromeDriverManager().install())
-driver = webdriver.Chrome(r"C:\Users\yaros\chromedriver_win32\chromedriver.exe")
+driver = webdriver.Chrome(ChromeDriverManager().install())
+#driver = webdriver.Chrome(r"C:\Users\yaros\chromedriver_win32\chromedriver.exe")
 
 
 if __name__ == '__main__':
@@ -18,13 +19,13 @@ if __name__ == '__main__':
     input()
     soup = BeautifulSoup(driver.page_source, 'lxml')
 
-    for i in range(10, 305):
+    for i in range(200, 500):
         films = soup.find_all("div", class_="item _NO_HIGHLIGHT_")
         for film in films:
             raw_id = film['id']
             raw_id = raw_id[3:]
-            if idDB.add_link(raw_id, "redy") :
-                print("{} redy".format(raw_id))
+            if idDB.add_link(raw_id, "ready") :
+                print("{} ready".format(raw_id))
             else:
                 print("{} in DB".format(raw_id))
         # content = driver.find_element(by=By.XPATH, value="//*[@class='item _NO_HIGHLIGHT_']")
@@ -32,7 +33,6 @@ if __name__ == '__main__':
             "https://www.kinopoisk.ru/top/navigator/m_act[num_vote]/100/m_act[rating]/1%3A/order/rating/page/{}/#results".format(i))
         time.sleep(5)
         soup = BeautifulSoup(driver.page_source, 'lxml')
-        pass
 
 
 
