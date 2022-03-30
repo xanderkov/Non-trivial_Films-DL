e896781d89ff3af7e7fb36ef8793406638e108af
yarosshest <yarosshest@gmail.com>
2022-03-31 00:05:20 +0300

    Parsed all id

--- parsingSelenium.py
+++ parsingSelenium.py
@@ -8,14 +8,14 @@ from constants import PATH_CHROME
 options = webdriver.ChromeOptions()
 options.binary_location = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
 chrome_driver_binary = "D:\chromedriver_win32\chromedriver.exe"
-driver = webdriver.Chrome(ChromeDriverManager().install())
-#driver = webdriver.Chrome(r"C:\Users\yaros\chromedriver_win32\chromedriver.exe")
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome(PATH_CHROME)
 
 
 if __name__ == '__main__':
     idDB = DataFunId()
     driver.set_window_size(1920, 1080)
-    driver.get("https://www.kinopoisk.ru/top/navigator/m_act[num_vote]/100/m_act[rating]/1%3A/order/rating/page/1/#results")
+    driver.get("https://www.kinopoisk.ru/top/navigator/m_act[num_vote]/100/m_act[rating]/1%3A/order/rating/perpage/200/#results")
     print("Enter the capcha , after input smf")
     input()
     soup = BeautifulSoup(driver.page_source, 'lxml')
@@ -29,7 +29,7 @@ if __name__ == '__main__':
             if idDB.add_link(raw_id, "ready") :
                 print("{} ready".format(raw_id))
             else:
-                print("{} in DB".format(raw_id))
+                print("{} already in DB".format(raw_id))
         # content = driver.find_element(by=By.XPATH, value="//*[@class='item _NO_HIGHLIGHT_']")
         driver.get(
             "https://www.kinopoisk.ru/top/navigator/m_act[num_vote]/100/m_act[rating]/1%3A/order/rating/page/{}/#results".format(i))
