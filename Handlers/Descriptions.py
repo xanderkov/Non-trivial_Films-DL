ccdc88e138c024cb9cd7c57673abcc8e36d3cdf8
yarosshest <yarosshest@gmail.com>
2022-04-08 21:43:43 +0300

    threading didn't work

--- Handlers/Descriptions.py
+++ Handlers/Descriptions.py
@@ -20,31 +20,34 @@ class Model(object):
         wget.download(self.model_url)
 
     def getDistance(self, first, second):
-        return self.model.wmdistance(first, second)
+        return self.model.wmdistance(first, second,  norm=True)
 
 
-def moreSimilar(films, id, prepDisc, model, done_ids):
+def moreSimilar(films, id, prepDisc, model):
     lst = []
     for i in films:
-        if (i[0] != id) and (not(id in done_ids)):
+        if i[0] != id:
             d = model.getDistance(i[1], prepDisc)
             if not(math.isinf(d)):
                 lst.append([d, i[0]])
     lst.sort(key=lambda x: x[0])
+    lst = lst[:100]
     return lst
 
 
 def prep_dist(films, model, db):
     with open('done_ids.pkl', 'rb') as f:
         done_ids = pickle.load(f)
-
-    for i in tqdm(films):
-        if not (i[0] in done_ids):
-            dist = moreSimilar(films, i[0], i[1], model, done_ids)
-            db.addDistance(i[0], dist)
-            done_ids.append(i[0])
-            with open('done_ids.pkl', 'wb') as f:
+        f.close()
+    with open('done_ids.pkl', 'wb') as f:
+        for i in tqdm(films):
+            if not (i[0] in done_ids):
+                dist = moreSimilar(films, i[0], i[1], model)
+                db.addDistance(i[0], dist)
+                done_ids.append(i[0])
                 pickle.dump(done_ids, f)
+    f.close()
+
 
 def main():
     dbFilm = DataFunFilm()
